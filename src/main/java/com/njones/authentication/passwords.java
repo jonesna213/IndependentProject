9db416a4d49cff6eb93a7ed4084fbf493d357186
jonesna213 <navyjones9@gmail.com>
2022-09-15 18:08:52 -0500

    Created class for passwords

---
+++ src/main/java/com/njones/authentication/passwords.java
@@ -0,0 +1,51 @@
+package com.njones.authentication;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+/**
+ * This class is for either creating or checking that a password matches.
+ *
+ * I found this on https://howtodoinjava.com/java/java-security/how-to-generate-secure-password-hash-md5-sha-pbkdf2-bcrypt-examples/
+ * Reason I used already created code was because I was not taught password hashing in java yet.
+ */
+public class passwords {
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+    public String getPassword(String password) throws NoSuchAlgorithmException {
+        String salt = getSalt();
+
+        return get_SHA_512_SecurePassword(password, salt);
+    }
+
+    private String get_SHA_512_SecurePassword(String passwordToHash,
+                                                     String salt) {
+        String generatedPassword = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            md.update(salt.getBytes());
+            byte[] bytes = md.digest(passwordToHash.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for (byte aByte : bytes) {
+                sb.append(Integer.toString((aByte & 0xff) + 0x100, 16)
+                        .substring(1));
+            }
+            generatedPassword = sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            logger.error("Error generating password: ", e);
+        }
+        return generatedPassword;
+    }
+
+    // Add salt
+    private static String getSalt() throws NoSuchAlgorithmException {
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+        byte[] salt = new byte[16];
+        sr.nextBytes(salt);
+        return salt.toString();
+    }
+}
